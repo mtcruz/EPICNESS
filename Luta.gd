4debc737cb5a7769a19228a58029b7b714f64981
Mateus Torres Cruz <mateustorrescruz22@gmail.com>
2023-07-08 13:47:34 -0300

    Disable move button if there isn't enough stamina

--- Luta.gd
+++ Luta.gd
@@ -27,9 +27,40 @@ func _process(_delta):
 	$HeroStamina.set_text("💪 %d" % [stamina_hero])
 	$VillainStamina.set_text("💪 %d" % [stamina_villain])
 	$Turns.set_text("%d / %d" % [epicness_current, epicness_max])
+	check_stamina()
 	check_damage()
 	check_gameover()
 
+
+# Deactivates buttons in case there is not enough stamina
+func check_stamina(): # Beware, this is ugly[]
+	if stamina_villain < 3: # Villain's move 1
+		$villain_move_1.disabled = true
+	else:
+		$villain_move_1.disabled = false
+		
+	if stamina_villain < 5: # Villain's move 2
+		$villain_move_2.disabled = true
+	else:
+		$villain_move_2.disabled = false
+		
+	if stamina_hero < 1: # Hero's move 1
+		$hero_move_1.disabled = true
+	else:
+		$hero_move_1.disabled = false
+
+	if stamina_hero < 3: # Hero's move 2
+		$hero_move_2.disabled = true
+	else:
+		$hero_move_2.disabled = false
+
+	if stamina_hero < 1: # Hero's move 3
+		$hero_move_3.disabled = true
+	else:
+		$hero_move_3.disabled = false
+
+
+# Calculates combat variables and applies them
 func check_damage():
 	if actor == "hero":
 		health_hero += health_regen
@@ -43,6 +74,16 @@ func check_damage():
 		stamina_hero -= stamina_damage
 		stamina_villain -= stamina_cost
 		action_was_taken = true
+	
+	# Prevent negative
+	if health_hero < 0:
+		health_hero = 0
+	if health_villain < 0:
+		health_villain = 0
+	if stamina_hero < 0:
+		stamina_hero = 0
+	if stamina_villain < 0:
+		stamina_villain = 0
 	
 	# Reset combat variables if action was taken
 	if action_was_taken:
@@ -83,7 +124,7 @@ func _on_villain_move_2(): # Kamikaze attack
 	stamina_cost = 6
 func _on_villain_move_3(): # Regenerate stamina
 	actor = "villain"
-	stamina_cost -6
+	stamina_cost = -6
 
 
 # Ganhando e perdendo ##########################################################
