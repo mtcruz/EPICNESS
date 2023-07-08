afcdb8255dc7a25fd00c37b79830d47de416fbd1
Mateus Torres Cruz <mateustorrescruz22@gmail.com>
2023-07-08 15:50:07 -0300

    Improve code order to increase readability

--- Luta.gd
+++ Luta.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
+
+# Initialize variables #########################################################
+# Used for winning conditions
+var epicness_current = 0
+var epicness_max = 6
+var epicness_reached_peak = false
+var stage_is_over = false
+# used for combat calculations
 var health_hero = 6
 var health_hero_max = 6
 var health_villain = 6
@@ -8,18 +16,14 @@ var stamina_hero = 6
 var stamina_hero_max = 6
 var stamina_villain = 6
 var stamina_villain_max = 6
-var epicness_current = 0
-var epicness_max = 6
-var epicness_reached_peak = false
-var game_is_over = false
-var actor # the character making the action
-var action_was_taken # a character made an action?
-
-# used for combat calculations
 var health_regen = 0
 var health_damage = 0
 var stamina_cost = 0
 var stamina_damage = 0
+var actor # the character making the action
+var action_was_taken # a character made an action?
+# Stage-related
+
 
 # atualiza variáveis na tela a cada frame
 func _process(_delta):
@@ -34,11 +38,12 @@ func _process(_delta):
 		$SubMessage.set_text("End the battle now!")
 	else:
 		$Turns.set_text("%d / %d" % [epicness_current, epicness_max])
+		$Message.set_text(current_goal)
 	check_stamina()
 	check_damage()
 	check_gameover()
 
-# Combate ######################################################################
+# Combat #######################################################################
 
 # Deactivates buttons in case there is not enough stamina
 func check_stamina(): # Beware, this is ugly[]
@@ -113,7 +118,7 @@ func manage_epicness():
 		check_gameover()
 
 
-# Ganhando e perdendo ##########################################################
+# Winning and losing Ganhando e perdendo #######################################
 
 func check_gameover():
 	if epicness_current == epicness_max and health_villain == 0:
@@ -132,13 +137,13 @@ func win():
 		$SubMessage.text = str("The hero's sacrifice will not be forgotten!")
 	else:
 		$SubMessage.text = str("The villain was slain epically!")
-	game_is_over = true
+	stage_is_over = true
 	disable_buttons()
 
 func lose(): # Differentes mensagens a depender de quem morreu
-	$Turns.set_text("The epic moment passed")
 	$Message.text = str("You lose...")
 	if epicness_reached_peak:
+		$Turns.set_text("The epic moment passed")
 		$SubMessage.text = str("The Monarch lost its interest in the battle")
 	elif epicness_current != epicness_max:
 		$SubMessage.text = str("The battle wasn't epic enough")
@@ -146,7 +151,7 @@ func lose(): # Differentes mensagens a depender de quem morreu
 		$SubMessage.text = str("The villain died before the battle got epic")
 	elif health_hero <= 0:
 		$SubMessage.text = str("The Hero died")
-	game_is_over = true
+	stage_is_over = true
 	disable_buttons()
 
 func disable_buttons():
@@ -158,7 +163,7 @@ func disable_buttons():
 	$villain_move_3.disabled = true
 
 
-# Habilidades de cada personagem ###############################################
+# Character movesets ###########################################################
 # Buttons for the Hero moveset 
 func _on_hero_move_1(): # Weak attack
 	actor = "hero"
@@ -189,7 +194,7 @@ func _on_villain_move_3(): # Regenerate stamina
 	stamina_cost = -6
 
 
-# Funcionalidades ##############################################################
+# etc ##########################################################################
 
 func _on_reload_button_down():
 	get_tree().reload_current_scene()
