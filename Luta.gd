d86dbe733c99a585ad2a13578356d1a347e95642
Mateus Torres Cruz <mateustorrescruz22@gmail.com>
2023-07-08 22:21:13 -0300

    General bugfixing

--- Luta.gd
+++ Luta.gd
@@ -21,10 +21,12 @@ var health_damage = 0
 var stamina_cost = 0
 var stamina_damage = 0
 var actor # the character making the action
+var last_actor = "Villain"
 var action_was_taken # a character made an action?
 # Stage-related
 var current_stage = 1
 var stage_is_set = false
+var game_over = false
 # Timings
 var time_started
 var time_now = 0
@@ -34,6 +36,7 @@ var time_elapsed = 0
 
 func _ready():
 	$TransitionMessage.visible = false
+	$DebugInfo.visible = false
 	time_started = Time.get_unix_time_from_system()
 
 
@@ -52,6 +55,8 @@ func _process(_delta):
 	var time_since_transition = time_elapsed - time_transition
 	if time_since_transition > 5:
 		$TransitionMessage.visible = false
+	if time_since_transition > 4 and game_over == true:
+		get_tree().reload_current_scene()
 	
 	# atualiza variáveis na tela a cada frame
 	$DebugInfo.set_text(str(" | current_stage: ", current_stage, \
@@ -61,8 +66,9 @@ func _process(_delta):
 							" | epicness_reached_peak: ", epicness_reached_peak, \
 							" | stage_is_over: ", stage_is_over, \
 							"\n | time_elapsed: ", time_elapsed,
-							"\n | time_transition " , time_transition,
-							"\n | time_now " , time_now))
+							"\n | time_transition: " , time_transition,
+							"\n | time_now: " , time_now, \
+							" | last_actor: ", last_actor))
 	$HeroHealth.set_text("❤ %d" % [health_hero])
 	$VillainHealth.set_text("🖤 %d" % [health_villain])
 	$HeroStamina.set_text("💪 %d" % [stamina_hero])
@@ -77,7 +83,7 @@ func _process(_delta):
 	
 	# Roda funções de atualização
 	check_stamina()
-	check_damage()
+	#check_damage()
 	check_gameover()
 
 # The stages ###################################################################
@@ -91,7 +97,16 @@ func set_stage():
 	if current_stage == 2:
 		epicness_max = 2
 		$Message.text = "Stage 2"
-		$SubMessage.text = "Reach max epicness with a final blow on the Villain"
+		$SubMessage.text = "Now in only two epicness"
+	
+	if current_stage == 3:
+		epicness_max = 6
+		$Message.text = "Stage 3"
+		$SubMessage.text = "Now alternate the characters after each move"
+	
+	if current_stage == 4:
+		await get_tree().create_timer(4.0).timeout
+		get_tree().quit()
 	
 	health_hero = health_hero_max
 	stamina_hero = stamina_hero_max
@@ -110,29 +125,27 @@ func set_stage():
 # Deactivates buttons in case there is not enough stamina
 func check_stamina(): # Beware, this is ugly[]
 	if stamina_villain < 3: # Villain's move 1
-		$villain_move_1.disabled = true
+		$VillainButtons/villain_move_1.disabled = true
 	else:
-		$villain_move_1.disabled = false
+		$VillainButtons/villain_move_1.disabled = false
 		
 	if stamina_villain < 5: # Villain's move 2
-		$villain_move_2.disabled = true
+		$VillainButtons/villain_move_2.disabled = true
 	else:
-		$villain_move_2.disabled = false
+		$VillainButtons/villain_move_2.disabled = false
 		
 	if stamina_hero < 1: # Hero's move 1
-		$hero_move_1.disabled = true
+		$HeroButtons/hero_move_1.disabled = true
 	else:
-		$hero_move_1.disabled = false
-
+		$HeroButtons/hero_move_1.disabled = false
 	if stamina_hero < 3: # Hero's move 2
-		$hero_move_2.disabled = true
+		$HeroButtons/hero_move_2.disabled = true
 	else:
-		$hero_move_2.disabled = false
-
+		$HeroButtons/hero_move_2.disabled = false
 	if stamina_hero < 1: # Hero's move 3
-		$hero_move_3.disabled = true
+		$HeroButtons/hero_move_3.disabled = true
 	else:
-		$hero_move_3.disabled = false
+		$HeroButtons/hero_move_3.disabled = false
 
 
 # Calculates combat variables and applies them
@@ -167,6 +180,7 @@ func check_damage():
 		stamina_cost = 0
 		stamina_damage = 0
 		action_was_taken = false
+		last_actor = actor
 		actor = "none"
 		manage_epicness()
 
@@ -175,7 +189,7 @@ func check_damage():
 func manage_epicness():
 	epicness_current += 1
 	
-	if epicness_current == 6:
+	if epicness_current == epicness_max:
 		epicness_reached_peak = true
 
 
@@ -198,19 +212,32 @@ func win():
 		$TransitionMessage/Line3.text = str("The hero's sacrifice will not be forgotten!")
 	else:
 		$TransitionMessage/Line3.text = str("The villain was slain epically!")
+	if current_stage == 4:
+		$TransitionMessage/Line1.text = str("You finshed the game!")
+		$TransitionMessage/Line2.set_text("Thanks for playing")
+		$TransitionMessage/Line3.text = str("The game will now close")
+		$TransitionMessage/Line4.text = str("")
+		
 	end_stage()
 
+
+
 func lose(): # Differentes mensagens a depender de quem morreu
-	$TransitionMessage/Line1.text = str("You lose...")
+	$TransitionMessage/Line1.text = str("Game Over")
 	if epicness_reached_peak:
 		$TransitionMessage/Line2.set_text("The epic moment passed")
 		$TransitionMessage/Line3.text = str("The Monarch lost its interest in the battle")
 	elif epicness_current != epicness_max:
-		$TransitionMessage/Line3.text = str("The battle wasn't epic enough")
+		$TransitionMessage/Line2.text = str("The battle wasn't epic enough")
+		$TransitionMessage/Line3.text = str("")
 	elif health_villain <= 0:
-		$TransitionMessage/Line3.text = str("The villain died before the battle got epic")
+		$TransitionMessage/Line2.text = str("The villain died before the battle got epic")
+		$TransitionMessage/Line3.text = str("")
 	elif health_hero <= 0:
-		$TransitionMessage/Line3.text = str("The Hero died")
+		$TransitionMessage/Line2.text = str("The Hero died")
+		$TransitionMessage/Line3.text = str("")
+	$TransitionMessage/Line4.text = str("Game Over. The game will restart in 5 seconds")
+	game_over = true
 	end_stage()
 
 func end_stage():
@@ -225,22 +252,41 @@ func end_stage():
 	#stage_menu_interacted
 
 func disable_buttons():
-	$hero_move_1.disabled = true
-	$hero_move_2.disabled = true
-	$hero_move_3.disabled = true
-	$villain_move_1.disabled = true
-	$villain_move_2.disabled = true
-	$villain_move_3.disabled = true
+	$HeroButtons/hero_move_1.disabled = true
+	$HeroButtons/hero_move_2.disabled = true
+	$HeroButtons/hero_move_3.disabled = true
+	$VillainButtons/villain_move_1.disabled = true
+	$VillainButtons/villain_move_2.disabled = true
+	$VillainButtons/villain_move_3.disabled = true
+
+func disable_buttons_hero():
+	$HeroButtons/hero_move_1.visible = false
+	$HeroButtons/hero_move_2.visible = false
+	$HeroButtons/hero_move_3.visible = false
+	
+func disable_buttons_villain():
+	$VillainButtons/villain_move_1.visible = false
+	$VillainButtons/villain_move_2.visible = false
+	$VillainButtons/villain_move_3.visible = false
 
 
 func enable_buttons():
-	$hero_move_1.disabled = false
-	$hero_move_2.disabled = false
-	$hero_move_3.disabled = false
-	$villain_move_1.disabled = false
-	$villain_move_2.disabled = false
-	$villain_move_3.disabled = false
+	$HeroButtons/hero_move_1.disabled = false
+	$HeroButtons/hero_move_2.disabled = false
+	$HeroButtons/hero_move_3.disabled = false
+	$VillainButtons/villain_move_1.disabled = false
+	$VillainButtons/villain_move_2.disabled = false
+	$VillainButtons/villain_move_3.disabled = false
 
+func enable_buttons_hero():
+	$HeroButtons/hero_move_1.visible = true
+	$HeroButtons/hero_move_2.visible = true
+	$HeroButtons/hero_move_3.visible = true
+
+func enable_buttons_villain():
+	$VillainButtons/villain_move_1.visible = true
+	$VillainButtons/villain_move_2.visible = true
+	$VillainButtons/villain_move_3.visible = true
 
 # Character movesets ###########################################################
 # Buttons for the Hero moveset 
@@ -248,35 +294,69 @@ func _on_hero_move_1(): # Weak attack
 	actor = "hero"
 	health_damage = 2
 	stamina_cost = 1
+	if current_stage == 3:
+		disable_buttons_hero()
+		enable_buttons_villain()
+	check_damage()
 func _on_hero_move_2(): # Strong attack
 	actor = "hero"
 	health_damage = 4
 	stamina_damage = 1
 	stamina_cost = 3
+	if current_stage == 3:
+		disable_buttons_hero()
+		enable_buttons_villain()
+	check_damage()
 func _on_hero_move_3(): # Heal self
 	actor = "hero"
 	health_regen = 2
 	stamina_cost = 2
+	if current_stage == 3:
+		disable_buttons_hero()
+		enable_buttons_villain()
+	check_damage()
 
 # Buttons for the Villain moveset 
 func _on_villain_move_1(): # Strong attack
 	actor = "villain"
 	health_damage = 3
 	stamina_cost = 3
+	if current_stage == 3:
+		disable_buttons_villain()
+		enable_buttons_hero()
+	check_damage()
 func _on_villain_move_2(): # Kamikaze attack
 	actor = "villain"
 	health_regen = -3
 	health_damage = 5
 	stamina_cost = 6
+	if current_stage == 3:
+		disable_buttons_villain()
+		enable_buttons_hero()
+	check_damage()
 func _on_villain_move_3(): # Regenerate stamina
 	actor = "villain"
 	stamina_cost = -6
+	if current_stage == 3:
+		disable_buttons_villain()
+		enable_buttons_hero()
+	check_damage()
 
 
 # etc ##########################################################################
 
 func _on_reload_button_down():
-	get_tree().reload_current_scene()
+	health_hero = health_hero_max
+	stamina_hero = stamina_hero_max
+	health_villain = health_villain_max
+	stamina_villain = stamina_villain_max
+	epicness_current = 0
+	last_actor = "none"
+	enable_buttons_hero()
+	enable_buttons_villain()
 
 func _on_quit_button_down():
 	get_tree().quit()
+
+func _on_restart_all_button_down():
+	get_tree().reload_current_scene()
